4862fe1be2f785a9a49d9ee246f19c9c0302052a
kingdomshiftmediacorp-pixel <kingdomshiftmediacorp@gmail.com>
2026-05-24 18:37:59 -1000

    Delete src directory

--- src/src/src/src/App.jsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div style={{ padding: "40px", background: "#0a0907", minHeight: "100vh", color: "white" }}>
-      <h1 style={{ color: "#c8a96e" }}>KingdomShift Marketplace Platform</h1>
-      <p>Your platform is connected to Vercel.</p>
-    </div>
-  )
-}
